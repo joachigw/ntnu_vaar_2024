c512ebfc6d776b89c72e0de12221d73a6e409bdf
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-17 20:12:47 +0100

    Add method for finding by user id and add repo annotation

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/ExpressionRepository.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/ExpressionRepository.java
@@ -2,7 +2,13 @@ package idatt2105.oving5.repository;
 
 import idatt2105.oving5.model.Expression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ExpressionRepository
         extends JpaRepository<Expression, Integer> {
+
+    List<Expression> findByAppUserId(Integer appUserId);
 }
