e49d523b110e4265b5d6694dc7f284241ab16335
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-17 20:09:41 +0100

    Add repository annotation

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/AppUserRepository.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/AppUserRepository.java
@@ -2,8 +2,10 @@ package idatt2105.oving5.repository;
 
 import idatt2105.oving5.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface AppUserRepository
         extends JpaRepository<AppUser, Integer> {
 
