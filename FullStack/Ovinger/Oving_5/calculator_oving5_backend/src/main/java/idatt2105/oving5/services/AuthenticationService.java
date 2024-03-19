6be8396dabbdbde83450feee15a2bf772416f330
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 13:33:06 +0100

    Add user login

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/services/AuthenticationService.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/services/AuthenticationService.java
@@ -1,14 +1,20 @@
 package idatt2105.oving5.services;
 
+import idatt2105.oving5.dto.LoginResponseDTO;
 import idatt2105.oving5.model.Role;
 import idatt2105.oving5.model.User;
 import idatt2105.oving5.repository.RoleRepository;
 import idatt2105.oving5.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -18,16 +24,20 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder encoder;
+    private final AuthenticationManager authManager;
+    private final TokenService tokenService;
 
     public AuthenticationService(UserRepository userRepository, RoleRepository roleRepository,
-                                 PasswordEncoder encoder) {
+                                 PasswordEncoder encoder, AuthenticationManager authManager,
+                                 TokenService tokenService) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.encoder = encoder;
+        this.authManager = authManager;
+        this.tokenService = tokenService;
     }
 
     public User registerUser(String username, String password) {
-
         String encodedPassword = encoder.encode(password);
         Role userRole = null;
         if (roleRepository.findByAuthority("USER").isPresent()) userRole = roleRepository.findByAuthority("USER").get();
@@ -36,5 +46,24 @@ public class AuthenticationService {
         authorities.add(userRole);
 
         return userRepository.save(new User(username, encodedPassword, authorities));
+    }
+
+    public LoginResponseDTO loginUser(String username, String password) {
+        try {
+            Authentication auth = authManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            String token = tokenService.generateJwt(auth);
+
+            Optional<User> optionalUser = userRepository.findByUsername(username);
+
+            if (optionalUser.isPresent()) {
+                return new LoginResponseDTO(optionalUser.get(), token);
+            }
+
+        } catch (AuthenticationException e) {
+            // TODO: Return proper exception
+            return new LoginResponseDTO(null, "");
+        }
+
+        return null;
     }
 }
