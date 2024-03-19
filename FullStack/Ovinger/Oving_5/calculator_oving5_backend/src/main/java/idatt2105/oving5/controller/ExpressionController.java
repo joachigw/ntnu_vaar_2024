117c1b4743d552ffff0b89f35b099fc188d5fc15
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 11:08:23 +0100

    Add new endpoint path

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/ExpressionController.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/ExpressionController.java
@@ -1,8 +1,8 @@
 package idatt2105.oving5.controller;
 
-import idatt2105.oving5.model.AppUser;
+import idatt2105.oving5.model.User;
 import idatt2105.oving5.model.Expression;
-import idatt2105.oving5.services.AppUserService;
+import idatt2105.oving5.services.UserService;
 import idatt2105.oving5.services.ExpressionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +10,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/expressions")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ExpressionController {
 
     private final ExpressionService expressionService;
-    private final AppUserService appUserService;
+    private final UserService appUserService;
 
-    public ExpressionController(ExpressionService expressionService, AppUserService appUserService) {
+    public ExpressionController(ExpressionService expressionService, UserService userService) {
         this.expressionService = expressionService;
-        this.appUserService = appUserService;
+        this.appUserService = userService;
     }
 
     @GetMapping("/")
@@ -25,15 +26,15 @@ public class ExpressionController {
         return expressionService.findAllExpressions();
     }
 
-    @GetMapping("/appuser")
-    public List<Expression> getExpressions(@RequestBody AppUser appUser) {
-        Integer appUserId = appUserService.findAppUserByUsername(appUser.getUsername()).getId();
-        return expressionService.findAllExpressionsByAppUserId(appUserId);
-    }
-
     @PostMapping("/")
     public void addExpression(@RequestBody Expression expression) {
         expressionService.addExpression(expression);
+    }
+
+    @GetMapping("/user")
+    public List<Expression> getExpressions(@RequestBody User appUser) {
+        Integer appUserId = appUserService.findAppUserByUsername(appUser.getUsername()).getId();
+        return expressionService.findAllExpressionsByAppUserId(appUserId);
     }
 
     @PutMapping("/{expressionId}/appuser/{appUserId}")
