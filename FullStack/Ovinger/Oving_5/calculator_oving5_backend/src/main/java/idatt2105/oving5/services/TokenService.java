9eb84555082754c86b889959b74a924a136c0d84
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 13:37:16 +0100

    Initial, generate JWT with 5 min expiration timer

---
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/services/TokenService.java
@@ -0,0 +1,40 @@
+package idatt2105.oving5.services;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+
+    public TokenService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public String generateJwt(Authentication auth) {
+        Instant now = Instant.now();
+        Instant expiresAt = now.plusSeconds(300);
+
+        String scope = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .subject(auth.getName())
+                .claim("roles", scope)
+                .expiresAt(expiresAt)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
