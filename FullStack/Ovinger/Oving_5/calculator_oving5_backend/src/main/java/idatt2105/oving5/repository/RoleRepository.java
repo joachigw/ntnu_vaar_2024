844bf47523752415ec9c33b20ba157a7e3a7bc1f
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 11:13:34 +0100

    Initial findByAuthority

---
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package idatt2105.oving5.repository;
+
+import idatt2105.oving5.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByAuthority(String authority);
+}
