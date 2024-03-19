ec9f811e823efa0236ba904032cfd17fb81e8f9c
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 13:32:41 +0100

    Initial, register and login user through service class

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/AuthenticationController.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/AuthenticationController.java
@@ -1,4 +1,30 @@
 package idatt2105.oving5.controller;
 
+import idatt2105.oving5.dto.LoginResponseDTO;
+import idatt2105.oving5.dto.UserRegistrationDTO;
+import idatt2105.oving5.model.User;
+import idatt2105.oving5.services.AuthenticationService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody UserRegistrationDTO registrationDTO) {
+        //TODO: Remove return of password, use DTO
+        return authService.registerUser(registrationDTO.getUsername(), registrationDTO.getPassword());
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody UserRegistrationDTO registrationDTO) {
+        return authService.loginUser(registrationDTO.getUsername(), registrationDTO.getPassword());
+    }
 }
