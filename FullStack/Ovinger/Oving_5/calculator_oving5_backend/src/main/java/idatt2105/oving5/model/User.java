66ce21d62c7cc929610b022ee56bba022617eb27
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 11:10:56 +0100

    Implement UserDetails, add column restraints, constructors and new toString

---
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/model/User.java
@@ -0,0 +1,140 @@
+package idatt2105.oving5.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="app_user")
+public class User implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+            name = "appuser_id_sequence",
+            sequenceName = "appuser_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "appuser_id_sequence"
+    )
+    @Column(name = "app_user_id", unique = true)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "appUser")
+    private Set<Expression> expressions;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "app_user_role",
+            joinColumns = {@JoinColumn(name = "app_user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
+
+    public User() {
+        this.authorities = new HashSet<>();
+        this.expressions = new HashSet<>();
+    }
+
+    public User(String username, String password, Set<Role> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public User(Integer id, String username, String password, Set<Role> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Expression> getExpressions() {
+        return expressions;
+    }
+
+    public void setExpressions(Set<Expression> expressions) {
+        this.expressions = expressions;
+    }
+
+
+    // Spring security
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", expressions=" + expressions +
+                ", authorities=" + authorities.toString() +
+                '}';
+    }
+}
