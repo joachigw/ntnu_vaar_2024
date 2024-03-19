b0a1e7e5c3d07a1c09bf60a67eb70b615fa1e2dc
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 13:34:32 +0100

    Add hello message as standard GET

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/UserController.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/UserController.java
@@ -1,13 +1,11 @@
 package idatt2105.oving5.controller;
 
-import idatt2105.oving5.model.Role;
 import idatt2105.oving5.model.User;
 import idatt2105.oving5.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -21,15 +19,15 @@ public class UserController {
         this.userService = userService;
     }
 
-//    @GetMapping("/")
-//    public String userHello() {
-//        return "User level access";
-//    }
-
     @GetMapping("/")
-    public List<User> getAppUsers() {
-        return userService.findAllAppUsers();
+    public String userHello() {
+        return "User level access";
     }
+
+//    @GetMapping("/")
+//    public List<User> getAppUsers() {
+//        return userService.findAllAppUsers();
+//    }
 
     @PostMapping("/")
     public Boolean addAppUser(@RequestBody User appUser) {
