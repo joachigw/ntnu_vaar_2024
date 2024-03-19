e8a0914fdee41f106a3cb716f2181fe6497918e1
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-19 13:37:36 +0100

    Initial, client format for user registration

---
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/dto/UserRegistrationDTO.java
@@ -0,0 +1,41 @@
+package idatt2105.oving5.dto;
+
+public class UserRegistrationDTO {
+
+    private String username;
+    private String password;
+
+    public UserRegistrationDTO() {
+        super();
+    }
+
+    public UserRegistrationDTO(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRegistrationDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
