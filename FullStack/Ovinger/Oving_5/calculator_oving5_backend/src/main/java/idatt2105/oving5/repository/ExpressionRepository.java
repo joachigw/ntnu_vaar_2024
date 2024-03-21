0178545520169f0bfcec479edd19688108b38724
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-21 10:24:10 +0100

    Add custom SQL-query

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/ExpressionRepository.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/repository/ExpressionRepository.java
@@ -2,11 +2,15 @@ package idatt2105.oving5.repository;
 
 import idatt2105.oving5.model.Expression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ExpressionRepository extends JpaRepository<Expression, Integer> {
-    List<Expression> findByAppUserId(Integer appUserId);
+    @Query("SELECT e.id, e.firstNumber, e.secondNumber, e.operator, e.result, u.id FROM Expression e " +
+            "LEFT JOIN User u ON e.appUser.id = u.id WHERE e.appUser.id = :appUserId")
+    List<Object[]> findByAppUserId(@Param("appUserId") Integer appUserId);
 }
