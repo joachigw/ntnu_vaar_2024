97667e7df56d24c78b8a7d036e160ac3932fde21
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-21 10:21:49 +0100

    Add functionality for mapping expression to user and save expression with calculated result in DB

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/CalculatorController.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/CalculatorController.java
@@ -1,16 +1,16 @@
 package idatt2105.oving5.controller;
 
+import idatt2105.oving5.dto.CalculationRequestDTO;
+import idatt2105.oving5.dto.CalculationResponseDTO;
 import idatt2105.oving5.model.Expression;
 import idatt2105.oving5.model.Calculator;
+import idatt2105.oving5.model.User;
 import idatt2105.oving5.services.ExpressionService;
+import idatt2105.oving5.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/calculator")
@@ -18,34 +18,41 @@ import java.util.Map;
 public class CalculatorController {
 
     private final ExpressionService expressionService;
+    private final UserService userService;
 
-    public CalculatorController(ExpressionService expressionService) {
+    public CalculatorController(ExpressionService expressionService, UserService userService) {
         this.expressionService = expressionService;
+        this.userService = userService;
     }
 
 
     @PostMapping("/calculate")
-    public ResponseEntity<?> calculateExpression(@RequestBody Expression expression) {
+    public CalculationResponseDTO calculateExpression(@RequestBody CalculationRequestDTO requestDTO) {
 
         Calculator calculator = new Calculator();
         Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
         try {
-            logger.info("Calculating the following expression: " + expression.toString());
+            Expression expression = requestDTO.getExpression();
+            User user = userService.findAppUserByUsername(requestDTO.getUser().getUsername());
+
+            logger.info("Calculating the following expression: " + expression);
 
             double result = calculator.calculate(expression);
             logger.info("Saving expression to database...");
+            expression.setResult(result);
             expressionService.addExpression(expression);
-            logger.info("Saved!");
+            expressionService.assignAppUserToExpression(expression, user);
+            logger.info("Saved expression with id " + expression.getId() + " to user " + user.getUsername());
 
-            logger.info("Sending the following result as response: {'result': " + result + "}");
+            CalculationResponseDTO responseDTO = new CalculationResponseDTO(expression);
+            logger.info("Sending the following response: " + responseDTO);
+            responseDTO.setResult(result);
 
-            Map<String, Double> response = Collections.singletonMap("result", result);
-
-            return ResponseEntity.ok(response);
+            return responseDTO;
         } catch (Exception e) {
             logger.error("There was an error with the calculation of the expression. Error: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return new CalculationResponseDTO();
         }
     }
 }
