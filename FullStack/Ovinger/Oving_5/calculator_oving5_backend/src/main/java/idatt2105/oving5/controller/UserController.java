7598f469cedd356405274453d3ef8527ed85cdf1
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2024-03-21 10:28:10 +0100

    Remove unnecessary login-type-methods

--- FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/UserController.java
+++ FullStack/Ovinger/Oving_5/calculator_oving5_backend/src/main/java/idatt2105/oving5/controller/UserController.java
@@ -2,9 +2,9 @@ package idatt2105.oving5.controller;
 
 import idatt2105.oving5.model.User;
 import idatt2105.oving5.services.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -12,33 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
-    private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+
     @GetMapping("/")
-    public String userHello() {
-        return "User level access";
-    }
-
-//    @GetMapping("/")
-//    public List<User> getAppUsers() {
-//        return userService.findAllAppUsers();
-//    }
-
-    @PostMapping("/")
-    public Boolean addAppUser(@RequestBody User appUser) {
-        logger.info("Adding new user: " + appUser);
-        userService.addAppUser(appUser);
-        return true;
-    }
-
-    @PostMapping("/login")
-    public Boolean loginUser(@RequestBody User user) {
-        logger.info("Verifying user: " + user);
-        return userService.verifyAppUser(user);
+    public List<User> getAppUsers() {
+        return userService.findAllAppUsers();
     }
 }
